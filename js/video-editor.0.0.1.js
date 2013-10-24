00bee25526eef6556e9f3b08dd6ca73220d74231
masimo <sho-max@yandex.ru>
2013-10-25 00:42:53 +0300

    commit

--- js/video-editor.0.0.1.js
+++ js/video-editor.0.0.1.js
@@ -4,17 +4,46 @@ $(function() {
     var selectors = {
         timeLine: $('.time_line'),
         play: $('#playIt'),
-        pause: $('#pause')
+        pause: $('#pause'),
+        nextClip: $('#nextClip'),
+        progressBar: $('.progress_bar')
+    }
+    var resorces = {
+        remote: 'http://stream.flowplayer.org/bauhaus/624x260.mp4',
+        local: '../media/video/oceans-clip.mp4'
     }
 
     var myPlayer = document.getElementById('video1'),
+        timeLine = null;
+
+    myPlayer.volume = 0.1;
 
 
     var editor = new VideoEditor(myPlayer);
 
     selectors.play.bind("click", function() {
+
         myPlayer.play();
-        myPlayer.currentTime = 15;
+
+        timeLine = setInterval(function() {
+            selectors.progressBar.html(Math.floor(myPlayer.currentTime));
+        }, 100);
+
+    });
+
+    selectors.nextClip.bind('click', function() {
+
+        console.log(myPlayer.currentTime);
+
+        if (myPlayer.currentTime >= 5) {
+            myPlayer.src = resorces.local;
+            myPlayer.play();
+            console.log(myPlayer.currentTime);
+
+            timeLine = setInterval(function() {
+                selectors.progressBar.html(parseInt(myPlayer.currentTime));
+            }, 100);
+        };
     });
 
     selectors.timeLine.bind("click", function(event) {
@@ -29,12 +58,15 @@ $(function() {
 
     selectors.pause.bind("click", function() {
         myPlayer.pause();
+        clearInterval(timeLine);
     });
 
-    myPlayer.addEventListener("durationchange", function(event) {
+    myPlayer.addEventListener("ratechange", function(event) {
 
         console.log(this.currentTime);
     });
+
+
 
 });
 
@@ -47,6 +79,9 @@ var VideoEditor = function(plaer) {
 
     self.updateTimeLine = function() {
 
+    };
+    self.updateProgress = function() {
+        
     };
 
 }
